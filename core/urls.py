1c3d609daec2c1dd0e8fb823f24e7238fa661eae
Seolheon <negnomy@gmail.com>
2024-04-30 18:29:40 +0500

    model product + note view current datetime

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 
 urlpatterns = [
     path('hello/', views.index),
+    path('date/', views.current_datetime),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def index(request):
     return HttpResponse('Hello!')
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>Текущая дата и время: {0}</body></html>".format(now)
+    return HttpResponse(html)
